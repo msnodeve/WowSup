dce60bcb36bba14377af9600ad1f4d8d018c8840
msnodeve <msnodeve@gmail.com>
2019-03-31 23:11:01 +0900

    그래들 추가 및 스타일 변경

--- WowSUP/app/src/main/java/com/seok/seok/wowsup/MainActivity.java
+++ WowSUP/app/src/main/java/com/seok/seok/wowsup/MainActivity.java
@@ -1,13 +1,18 @@
 package com.seok.seok.wowsup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
