d1e4162f6d0e80ee6d91918b32b8d576db0f02ed
msnodeve <msnodeve@gmail.com>
2019-04-02 21:41:49 +0900

    회원가입 페이지 구현 중

---
+++ WowSUP/app/src/main/java/com/seok/seok/wowsup/login/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.seok.seok.wowsup.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seok.seok.wowsup.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
